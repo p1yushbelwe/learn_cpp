ac97fc9df7f40031ed4df965435a5f9463ddc081
Piyush Belwe <pbelwe@gmail.com>
2025-06-20 15:26:34 +0530

    Learned about input and output using cin and cout, also extraction and insertion operators

---
+++ input_and_output/input_and_output.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Enter the value of num1\n";
+    cin >> num1; // loads value in num1
+    cout << "Enter the value of num2\n";
+    cin >> num2;
+    int sum = num1 + num2;
+    cout << "The sum of num1 and num2 is " << sum;
+    return 0;
+}
+
+/*
+>> this is called as extraction operator
+<< this is called as insertion operator
+WE USE COUT << FOR OUTPUT
+WE USE CIN << FOR INPUT
+
+
+
+*/
